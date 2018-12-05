0d011bfdc3b0b41206997696b7c40bf00e1a507e
Phạm Gia Vang <phamgiavang@gmail.com>
2018-12-05 20:35:09 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,35 @@
+<?php
+$msgtoto = "[!] tidak di ijinkan"."\n"."[!] menjual skrip ini kepada siapapun"."\n"."[!] msg by adi digitizer";
+$msgyd = "[!] enjoy your life";
+$msgwe = "[!] enjoy your life";
+
+$data= [
+'adi bordir',
+'Adi Digitizer','Dzi','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
+'Beuti Lavea','MasterCraft Vn','Ari Sandi','I love Cartoon','Kaboom','Robert Simangunsong','Mohamad Husn',
+'Lucy Thien','Adi Trader Demo','Mif Takhul','Feyy','I Dewa Gede Superatikadewa','Wendik','Qohwah',
+'Asmuri Aas','Goril Aas','Wili Aas','Gery Aas',
+'Redi Aas','Fenjul','Sabar Ajah','Dewi Tati','Fenjul',
+'Hasan','Yuli Iyul','Yun Yuyun','Davis','Novita',
+'$ FARHAN Z $','Muhammad Nandars','Muh Nandar',
+'Dep Bell','Adis Ahmat','Wali Roy','Roy Purba',
+'Acik Mall','Salim Aas','Bela Aas','Andrie',
+'Andriean','Andriesun','Acik Mall','Salim Aas',
+'Bela Aas','Rujana Bakong','Farid Mohamed','Dewi Aas',
+'Dila Aas','Bili Aas','Flybird Yuan','Teri Aas',
+'Den Aas','Amin Aas','Wulan Aas','Hasan Aas','Yusri','Fachrul Ejp','ajit vepal','Agus Riyadi',
+'Agus Riyadi Riyadi','Richard Steven Try',
+'Yoviar Pauzi','Ayesha Advocate',
+//vipm
+'HARIS Denzlot','Raihan Chainago','Bejo Jo206','Bejo Rusak','Jordan Donathan','Bejo Mace','Jojo Vake','Deaso Bejo','Lungkik Cuiru','Riyan Kartika','Lusto Rusak','Jorsn Rusak','Jordwun jerdin','Curit Astjx','Hais Suis',
+//mpiv
+'Gem Bel','Hati Yang Terluka','Umi Kholiifah',
+'Aditiya Sarofi Putra','NOOB GAMING','phim lẻ','Jordwun lrus','gan bro','Gan Bro','bot siji',
+'bot loro','bot telu','yu di','aryant259','Janandito W',
+'arifin','Noob Player','yoyomamam90@gmail.com','Andi Aas','elang aja','Muhammad Paijo','Sutejo','Siti Azizah','Dian Kartika','Budi Aas','Raby Aas',
+'Wendy Aas','Rani Aas','Lê Hoàng','Rere Aas','Rony Aas',
+'Riki Aas','Junaidi Abdulah','Tidak Dikenal','Agung Nugraha','SobLok DanSka VoltHz','Afil X-friends Discography Hitam-Putih','Aan TheRG','Davin Hardit','robert simangunsong','Restu OnGame','santa lidwyna tarigan','NEWBIE BERKELAS','Bambang Setiawan',
+'Mr. Benk','Yafia Ah','Yafiah Tri Ramdani wijaya','nayz stone','Myvi Com','Seda Coeg','Wayan','Kiki Albani',
+        ];
+$ydnews = ['tes',];
+$toto = ['tes',];
