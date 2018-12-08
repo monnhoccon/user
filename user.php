060c8ccb4b3514b838ae0a7fc5853411689fe2df
Phạm Gia Vang <phamgiavang@gmail.com>
2018-12-08 19:09:09 +0700

    Update user.php

--- user.php
+++ user.php
@@ -1,4 +1,7 @@
 <?php
+echo $red."[!] Chú ý !!!".$t;
+echo $kuning."[-] Bạn đang dùng tools".$t;
+echo $kuning."[-] Cảm ơn!".$t.$ijo.$t;
 
 $msgtoto = "[!] Chào bạn"."\n"."[!] Bạn đang sử dụng tools Toto"."\n"."[!] Việt hoá bởi PGV";
 $msgyd = "[!] enjoy your life";
@@ -6,12 +9,12 @@ $msgwe = "[!] enjoy your life";
 
 $data= [
 'Pham Gia Vang',
-'NhocConSr','semua andinya','sungging','asep kanceuh','putripesek','nubie305','ateng','Bal Lack','Maulana Setiawan','suga zora','Thauvan GT','yassar sha\'id shafi\'i','Moezza 01','စုံလင်သော အလ','hasan coc','Pro Master','hasan abut','gan bro','Yudi Pratmaja','Gembel Elite','imel telu','SkaLova','Badrun','Nhóc Con Sr','Delwin Yoanes',
+'NhocConSr','Nhóc Con Sr',
 
 //vipm
-'HARIS Denzlot','Raihan Chainago','Bejo Jo206','Bejo Rusak','Jordan Donathan','Bejo Mace','Jojo Vake','Deaso Bejo','Lungkik Cuiru','Riyan Kartika','Lusto Rusak','Jorsn Rusak','Jordwun jerdin','Curit Astjx','Hais Suis',
+'Pham Gia Vang',
 //mpiv
-'Gem Bel','Hati Yang Terluka','Umi Kholiifah',
+'Pham Gia Vang',
 
         ];
 $ydnews = ['tes',];
