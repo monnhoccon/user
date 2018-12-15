78ec8fd94b9bd66fc7778168f13a892498b57ba9
Phạm Gia Vang <phamgiavang@gmail.com>
2018-12-15 08:41:13 +0700

    Update user.php

--- user.php
+++ user.php
@@ -3,9 +3,9 @@ echo $red."[!] Chú ý !!!".$t;
 echo $kuning."[-] Bạn đang dùng tools Auto".$t;
 echo $kuning."[-] Chúc bạn kiếm được nhiều xèng!".$t.$ijo.$t;
 
-$msgtoto = "[!] Chào bạn"."\n"."[!] Bạn đang sử dụng tools Auto Toto"."\n"."[!] Việt hoá bởi PGV";
+$msgtoto = "[!] enjoy your life "."\n"."[!] Việt hoá bởi PGV";
 $msgyd = "[!] enjoy your life";
-$msgwe = "[!] update versi 1.3";
+$msgwe = "[!] enjoy your life";
 $msgveeu = "[!] enjoy your life";
 
 $data= [
