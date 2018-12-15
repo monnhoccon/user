ff3c95530459fe22547141ceca0d2c1c4290763b
Phạm Gia Vang <phamgiavang@gmail.com>
2018-12-16 05:17:45 +0700

    Update user.php

--- user.php
+++ user.php
@@ -2,7 +2,7 @@
 echo $red."[!] Chú ý !!!".$t;
 echo $kuning."[-] Bạn đang dùng tools Auto".$t;
 echo $kuning."[-] Chúc bạn kiếm được nhiều xèng!".$t.$ijo.$t;
-$ads= $putih."[÷]  Kênh Youtube    [÷]".$t.$ijo."[-] PGV ".$putih."| atau |".$ijo." sungging [-]";
+$ads= $putih."[÷]  Kênh Youtube    [÷]".$t.$ijo."[-] PGV ".$putih."| NhocCon.Sr |".$ijo." phamgiavang [-]";
 $msgtoto = "[!] enjoy your life "."\n"."[!] Việt hoá bởi PGV";
 $msgyd = "[!] enjoy your life";
 $msgwe = "[!] enjoy your life";
