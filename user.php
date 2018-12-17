58939f6a48606f61370284e6c8a30a5d3c6f378c
Phạm Gia Vang <phamgiavang@gmail.com>
2018-12-17 19:22:13 +0700

    Update user.php

--- user.php
+++ user.php
@@ -14,7 +14,7 @@ $data= [
 //vipm
 'Pham Gia Vang','Tịnh Tâm','Anh Phan','Kin Pham','Jey Pham','Chim Kut',
 //mpiv
-'Pham Vang','Kute AV','Vang Pham',
+'Pham Vang','Kute AV','Vang Pham','info yeumobile',
 
         ];
 $toto = ['tes',];
