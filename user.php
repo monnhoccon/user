2e931d80dc0cc793ed0b4191686314e3112daf10
Phạm Gia Vang <phamgiavang@gmail.com>
2018-12-21 21:52:50 +0700

    Update user.php

--- user.php
+++ user.php
@@ -10,7 +10,7 @@ $msgveeu = "[!] enjoy your life";
 
 $data= [
 'NhocConSr','Nhóc Con Sr','Nguyễn Thanh Thiên','Thông Trương','Nguyễn Thanh Quang','Trương Thị Hồng','Trương Hồng','Trương Văn Thông',
-
+'Trí Nguyễn Thành',
 //vipm
 'Pham Gia Vang','Tịnh Tâm','Anh Phan','Kin Pham','Jey Pham','Chim Kut',
 //mpiv
