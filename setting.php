cc7d283cabbcde031e49674e940be3eb4b581eb7
Phạm Gia Vang <phamgiavang@gmail.com>
2019-02-03 20:52:47 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -7,5 +7,8 @@ echo $kuning."[-] Liên hệ fb.com/NhocCon.Sr để lấy mật khẩu!".$t.$ij
 $pass="6ef5973f82e23b824683a0700bbc9bef";
 $key="343b84cae40e59926dbcd24e313e3b0b";
 
+$data= [
+'Pham Gia Vang',
+        ];
 
 ?>
