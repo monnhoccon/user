58e92cda1950b98ed18c4411f67b7f45ffde24b7
Phạm Gia Vang <phamgiavang@gmail.com>
2019-02-03 20:39:55 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,6 +1,9 @@
 <?php
 //author
-$creator="PGV";
+echo $red."[!] Chú ý !!!".$t;
+echo $kuning."[-] Bạn đang dùng tools Auto".$t;
+echo $kuning."[-] Nên cân nhắc khi sử dụng tools!".$t.$ijo.$t;
+
 $pass="6ef5973f82e23b824683a0700bbc9bef";
 $key="343b84cae40e59926dbcd24e313e3b0b";
 
