da4107ab0c7a4abf88f64d541b88a54358fa4045
Phạm Gia Vang <phamgiavang@gmail.com>
2019-02-03 20:19:39 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,1 +1,6 @@
-<?php/* setting bot */$baca_plus=$red."──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putihcreator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putihchanel :$kuning sungging$putih || code invite:$ijo F3NW34owm4".$t;$msg_baca= "[•] official chanel adi bordir".$t."[•] info bagi yang mengalami kesalahan saat login".$t."[•] silahkan download ulang skrip".$t."[•] kesalahan terdapat pada versi os lolipop";$stat_baca=true; $ccu= $red."──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────".$pth."──── █─ █─█ █ █─▀█──── █─ █─▄ █─█ █▀█──────── ─▀ ─▀─ ▀ ▀──▀──── ─▀ ▀▀▀ ─▀─ ▀▀─────creator: ".$ijo."adidoank ".$pth."|| chanel: ".$ijo."adi bordir".$t;$msg_ccu=$ijo."[•] official chanel adi bordir dan sungging";$stat_ccu=true;$mc=$br."──── █▄─▄█ ▄▀▄ █▄─█ █▀▀ ▀▄─▄▀──── ▄▀ █─█ █▀▄ █▀▀──────── █─█─█ █─█ █─▀█ █▀▀ ──█────── █─ █─█ █▀█ █▀▀──────── ▀───▀ ─▀─ ▀──▀ ▀▀▀ ──▀────── ─▀ ─▀─ ▀▀─ ▀▀▀────".$pth."creator: ".$ijo."adidoank ".$pth."|| ".$ijo."subcribe chanel adi bordir".$t.$pth."invite : ".$ijo."YCUSP ".$pth."|| ".$ijo."W9HVQ".$pth."support: ".$ijo."subcribe chanel sungging".$t;$msg_mc=$ijo."[•] enjoy your life";$stat_mc=;?>
+<?php
+
+
+
+
+?>
