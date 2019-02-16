a133b78bb81b0508fd9b2728c05f0b8f64604568
Phạm Gia Vang <phamgiavang@gmail.com>
2019-02-16 07:55:15 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,7 +2,7 @@
 //author
 echo $red."         [☆] Chú ý !!! [☆]".$t;
 echo $kuning."[-] Để sử dụng tools bạn cần nhập mật khẩu".$t;
-echo $kuning."[-] Liên hệ fb.com/NhocCon.Sr để lấy mật khẩu!".$t.$ijo.$r;
+echo $kuning."[-] Tham gia nhóm https://t.me/ToolScriptPGV để lấy mật khẩu!".$t.$ijo.$r;
 
 $pass="6ef5973f82e23b824683a0700bbc9bef";
 $key="343b84cae40e59926dbcd24e313e3b0b";
