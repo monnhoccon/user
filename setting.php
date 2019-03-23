03d1260df2adce81e11af78c410da57e403ff9cc
Phạm Gia Vang <phamgiavang@gmail.com>
2019-03-23 10:13:36 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -21,6 +21,7 @@ $epis="6ef5973f82e23b824683a0700bbc9bef";
 $iss="6ef5973f82e23b824683a0700bbc9bef";
 $ber="6ef5973f82e23b824683a0700bbc9bef";
 $vqdt="6ef5973f82e23b824683a0700bbc9bef";
+$ali="6ef5973f82e23b824683a0700bbc9bef";
 //Donate
 $donasi=false;
 $ads=$ijo.">> Subcribe channel Pham Gia Vang ".$t;
